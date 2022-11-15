caf5358b459c8139368c1e00557478663bb6d705
ikhlasdev17 <iklasaralbaev@gmail.com>
2022-11-15 22:51:45 +0500

    schedules started && some features added

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -1,10 +1,13 @@
 import HeaderLayout from '../../components/HeaderLayout'
+import Schedule from '../Schedule/Schedule'
 import Teachers from '../teacher/Teachers'
 export default function Dashboard () {
   return (
     <div className='container'>
       <HeaderLayout />
-      <div className='mt-12'>{/* <Teachers /> */}</div>
+      <div className='mt-6 bg-white p-4 rounded-md shadow-sm'>
+        <Schedule />
+      </div>
     </div>
   )
 }
